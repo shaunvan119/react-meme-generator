856356c6e61059e28e011bc9f8a4231451d02e2d
shaunvan119 <shaunvan@live.com.au>
2022-05-06 14:43:53 +1000

    added mem component and form input css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 
 
@@ -11,7 +12,8 @@ import Navbar from "./components/Navbar"
 export default function App() {
     return (
    <div>
-     <Navbar />
+     <Header />
+     <Meme />
    </div>
     
     )}

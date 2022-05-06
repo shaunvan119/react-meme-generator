f0078a4ded9c29b3b5af43e1cfb82a848539375f
shaunvan119 <shaunvan@live.com.au>
2022-05-06 13:09:51 +1000

    components folder and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Navbar from "./components/Navbar"
+
+
+
+
+
+
+
 
 
 export default function App() {
-    return <h1>Hello world!</h1>
-}
+    return (
+   <div>
+     <Navbar />
+   </div>
+    
+    )}
